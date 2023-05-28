de155cee99a8e5c8da571051874c0395dc025ace
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-05-28 19:44:01 +0300

    create table

---
+++ src/main/java/org/kadiraksoy/DbCreateTable.java
@@ -0,0 +1,30 @@
+package org.kadiraksoy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DbCreateTable {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/jdbc";
+        String name = "root";
+        String password= "1234";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, name, password);
+
+            String sorgu = "CREATE TABLE new-table (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50))";
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sorgu);
+
+            System.out.println("The new table was created successfully.");
+
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
