83e7901d0e09b2804a328208ba3a5f0becf25c66
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-05-28 19:42:28 +0300

    delete value

--- src/main/java/org/kadiraksoy/DbDeleteValue.java
+++ src/main/java/org/kadiraksoy/DbDeleteValue.java
@@ -1,5 +1,33 @@
 package org.kadiraksoy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class DbDeleteValue {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/jdbc";
+        String name = "root";
+        String password= "1234";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, name, password);
+
+            String query = "DELETE FROM user WHERE username = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, "ornek_kullanici");
+            preparedStatement.executeUpdate();
+
+            System.out.println("The user has been successfully deleted.");
+
+            preparedStatement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
